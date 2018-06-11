9f01feca4697a8d706b4621e91bcd111b13c3a7f
yamonephoopwintnaing <yamonenaing841995@gmail.com>
2018-06-11 21:00:26 +0630

    ex40.py

---
+++ ex40.py
@@ -0,0 +1,19 @@
+class Song(object):
+
+    def_init_(self, lyrics):
+       self.lyrics = lyrics
+    
+    def sing_me_a_song(self)
+        for line in self.lyrics:
+            print line
+
+happpy_bday = Song(["Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right there"])
+
+bullls_on_parade = Song(["They rally around the family",
+                         "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
