ea4dbb1f25c8dbb0e13286a6ae41b41a7458e0b3
yamonephoopwintnaing <yamonenaing841995@gmail.com>
2018-06-26 14:54:49 +0630

    change name

--- ex101.py
+++ ex101.py
@@ -1,15 +1,15 @@
-tabby-cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
+tabby_dog = "\tI'm tabbed in."
+persian_dog = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_dog = """
 I'll do a list:
-\t* Cat food
+    \t* Dog food
 \t* Fishies
 \t* Catnip\n\t* Grass
 """
 
-print tabby_cat
-print persian_cat
-print backslash_cat
-print fat_cat
+print tabby_dog
+print persian_dog
+print backslash_dog
+print fat_dog
